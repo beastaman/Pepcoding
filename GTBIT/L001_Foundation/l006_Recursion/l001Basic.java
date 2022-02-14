ac07e6e80eae5bdd10b0cc9b71accc466770fa48
Aman Kumar <iamaman12378@gmail.com>
2022-02-14 22:05:49 +0530

    update

---
+++ GTBIT/L001_Foundation/l006_Recursion/l001Basic.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class l001Basic{
+    private static Scanner scn = new Scanner(System.in);
+
+    public static void printDecreasing1(int n){
+        if(n == 0) return;
+        System.out.println("Hi"+n);
+        printDecreasing1(n-1);
+        System.out.println("Bye"+n);
+    }
+
+    // Find output when called for n = 5 ?
+    public static void fun(int n){
+        if(n == 0){
+            System.out.println("i have to stop" + n);
+            return;
+        }
+        for(int i=0;i<3;++i) System.out.print(n + "@" +i);
+        System.out.println();
+        if(n%2 == 0){
+            System.out.println("heading towards child function"+n);
+        }
+        fun(n-1);
+        if(n%2 != 0){
+            System.out.println("Back to parent function"+n);
+        }
+        
+    }
+
+    public static void printDecreasing(int n){
+        if(n==0) return ;
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
+
+    public static void printIncreasing(int n){
+        if(n==0) return ;
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
+
+    public static void printIncreasingDecreasing(int n){
+        if(n==0) return ;
+        System.out.println(n);
+        printIncreasingDecreasing(n-1);
+        System.out.println(n);
+    }
+    
+
+    public static void main(String[] args){
+        // printDecreasing(5);
+        // fun(5); //find output for this by dry run !
+
+    }
+}
